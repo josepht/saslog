c781276c58439926c94a39cc17de503dc898657f
Joe Talbott <joetalbott@gmail.com>
2016-04-01 16:44:28 -0400

    Make Logger more robust and flexible.
    * Add logger level data that's included in all logs
    * Add logger prefix that's included in all logs
    * Add type F to avoid a bunch of map[string]string instances
    * Get rid of the log() function and move that logic to Write()
      so that the Logger can be used as the io.Writer for the
      standard logger and include the prefix and default level.

--- saslog/saslog.go
+++ saslog/saslog.go
@@ -4,47 +4,71 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
-// Inspired by http://stackoverflow.com/a/36140590 Thu Mar 31 12:01:08 EDT 2016
-type logWriter struct {
-}
+type F map[string]string
 
-func (writer logWriter) Write(bytes []byte) (int, error) {
-	return fmt.Fprint(os.Stderr, time.Now().UTC().Format("2006-01-02 15:04:05.000")+string(bytes))
-}
+const (
+	DefaultLevel = "INFO"
+)
 
 type Logger struct {
-	l *log.Logger
+	l             *log.Logger
+	level         string
+	Data          F
+	IncludePrefix bool
+	prefix        string
+	tmpData       F
 }
 
-func (logger *Logger) Init() {
-	logger.l = log.New(new(logWriter), " SAS: ", 0)
+func New(prefix string, defaultLevel string, data F) *Logger {
+	l := new(Logger)
+	l.l = log.New(l, "", 0)
+	l.level = defaultLevel
+	l.Data = data
+	l.IncludePrefix = true
+	l.prefix = prefix
+	l.tmpData = F{}
+
+	return l
 }
 
-func (logger Logger) log(level string, msg string, data map[string]string) {
-	output := "\"" + msg + "\""
-	for key, value := range data {
+func (logger *Logger) Info(msg string, data F) {
+	logger.level = "INFO"
+	logger.tmpData = data
+	logger.l.Print(msg)
+}
+func (logger *Logger) Debug(msg string, data F) {
+	logger.level = "DEBUG"
+	logger.tmpData = data
+	logger.l.Print(msg)
+}
+
+func (logger *Logger) Write(bytes []byte) (int, error) {
+	output := time.Now().UTC().Format("2006-01-02 15:04:05.000")
+	if logger.IncludePrefix {
+		output += " " + logger.prefix
+	}
+
+	output += " " + logger.level + " "
+
+	// remove newline(s) added by log.Print
+	output += "\"" + strings.TrimSpace(string(bytes)) + "\""
+
+	for key, value := range logger.Data {
 		output += " " + key + "=" + value
 	}
-	logger.l.Print(level + " " + output)
-}
+	for key, value := range logger.tmpData {
+		output += " " + key + "=" + value
+	}
 
-func (logger Logger) Info(msg string, data map[string]string) {
-	logger.log("INFO", msg, data)
-}
-func (logger Logger) Debug(msg string, data map[string]string) {
-	logger.log("DEBUG", msg, data)
-}
+	// reset tmpData
+	logger.tmpData = F{}
 
-func main() {
-	logger := new(Logger)
-	logger.Init()
-
-	logger.Info("Info", map[string]string{
-		"file":  "blah",
-		"error": "none",
-	})
-	logger.Debug("Debug", nil)
+	w, e := fmt.Fprintln(os.Stderr, output)
+	// reset level
+	logger.level = DefaultLevel
+	return w, e
 }
